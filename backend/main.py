5b7e5d0daa391e1918fd4f71fc994cdee680c12a
Tuan Dao <dht.huytuan07@outlook.com>
2026-09-19 01:48:51 +0700

    add gitworkflow + fix some error using ruff

--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,20 @@
-from fastapi import FastAPI, UploadFile, HTTPException, File
+import os
+import subprocess
+import time
+
+import cv2
+from fastapi import FastAPI, File, HTTPException, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
 from ultralytics import YOLO
-import subprocess
-import json
-import time
-import os
-import cv2
 
-CORS_ORIGINS = os.getenv("CORS_ORIGINS","http://localhost:5173")
+CORS_ORIGINS = os.getenv("CORS_ORIGINS", "http://localhost:5173")
 app = FastAPI(title="Yolo Detection App")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = [CORS_ORIGINS],
-    allow_methods= {"*"},
-    allow_headers={"*"}
+    allow_origins=[CORS_ORIGINS],
+    allow_methods={"*"},
+    allow_headers={"*"},
 )
 
 MODEL_NAME = os.getenv("MODEL_NAME", "yolo26n.pt")
@@ -23,35 +23,37 @@ model = YOLO(MODEL_NAME)
 image_items = {"image/jpeg", "image/png"}
 video_items = {"video/mp4"}
 
-#health check
-@app.get ("/health")
-def health_check():
-    return {"status":"ok"}
 
-#detect image
-@app.post ("/api/detect/image") 
+# health check
+@app.get("/health")
+def health_check():
+    return {"status": "ok"}
+
+
+# detect image
+@app.post("/api/detect/image")
 async def upload_files(file: UploadFile = File(...)):
     if file.content_type not in image_items:
-        raise HTTPException (status_code=400, detail="Invalide file type")
+        raise HTTPException(status_code=400, detail="Invalide file type")
 
- #limit file size
-    file.file.seek(0,2)
+    # limit file size
+    file.file.seek(0, 2)
     file_size = file.file.tell()
     file.file.seek(0)
-    MAX_IMG_SIZE_MB = int(os.getenv("MAX_IMG_SIZE", 50))
+    MAX_IMG_SIZE_MB = int(os.getenv("MAX_IMG_SIZE", "50"))
     if file_size > MAX_IMG_SIZE_MB * 1024 * 1024:
         raise HTTPException(status_code=400, detail="File size too large!")
 
-    #save temporary image files
+    # save temporary image files
     contents = await file.read()
-    print (len(contents))
+    print(len(contents))
     temp_path = "temp_" + file.filename
-    with open (temp_path, "wb") as f:
+    with open(temp_path, "wb") as f:
         f.write(contents)
-   
-    #image detection process
+
+    # image detection process
     start_time = time.time()
-    results=model(temp_path)
+    results = model(temp_path)
     boxes = results[0].boxes
     print(boxes)
     detection = []
@@ -59,55 +61,69 @@ async def upload_files(file: UploadFile = File(...)):
         cls = model.names[int(boxes.cls[i])]
         conf = boxes.conf[i].item()
         xyxy = boxes.xyxy[i].tolist()
-        detection.append({"class": cls, "confidence": round(conf,2), "bbox": [round(coord,2) for coord in xyxy]})
+        detection.append(
+            {
+                "class": cls,
+                "confidence": round(conf, 2),
+                "bbox": [round(coord, 2) for coord in xyxy],
+            }
+        )
     count = len(boxes.cls)
     end_time = time.time()
-    time_result = (end_time - start_time)*1000
+    time_result = (end_time - start_time) * 1000
     annotated = results[0].plot()
     output_filename = "annotated" + file.filename
-    annotated_image = cv2.imwrite(filename=output_filename, img=annotated)
+    cv2.imwrite(filename=output_filename, img=annotated)
 
-    #Response w/ JSON format
-    response = ({
-        "count": count, 
+    # Response w/ JSON format
+    response = {
+        "count": count,
         "detection": detection,
         "time": time_result,
-        "annotated_image": output_filename
-        })
-    os.remove (temp_path)
-    return (response)
+        "annotated_image": output_filename,
+    }
+    os.remove(temp_path)
+    return response
 
 
-#detect videos
-@app.post ("/api/detect/video")
+# detect videos
+@app.post("/api/detect/video")
 async def detect_video(file: UploadFile):
-    #file type detect
+    # file type detect
     if file.content_type not in video_items:
         raise HTTPException(status_code=400, detail="Invalid file type")
 
-    #limit file size
-    file.file.seek(0,2)
+    # limit file size
+    file.file.seek(0, 2)
     file_size = file.file.tell()
     file.file.seek(0)
-    MAX_VIDEO_SIZE_MB = int(os.getenv("MAX_VIDEO_SIZE", 100))
+    MAX_VIDEO_SIZE_MB = int(os.getenv("MAX_VIDEO_SIZE", "100"))
     if file_size > MAX_VIDEO_SIZE_MB * 1024 * 1024:
         raise HTTPException(status_code=400, detail="File's size too large")
 
-    #save temporary file
+    # save temporary file
     contents = await file.read()
-    print (len(contents))
+    print(len(contents))
     temp_path = "temp_" + file.filename
-    with open (temp_path, "wb") as f:
+    with open(temp_path, "wb") as f:
         f.write(contents)
 
-    #video detection process
+    # video detection process
     result = model(temp_path)
 
     class_counts = {}
     output_video = cv2.VideoCapture(temp_path)
     raw_filename = "raw" + file.filename
     output_filename = "annotated" + file.filename
-    annotated_video = cv2.VideoWriter(filename = raw_filename, fourcc = cv2.VideoWriter_fourcc(*'mp4v'), fps = output_video.get(cv2.CAP_PROP_FPS), frameSize = (int(output_video.get(cv2.CAP_PROP_FRAME_WIDTH)), int(output_video.get(cv2.CAP_PROP_FRAME_HEIGHT))))
+    annotated_video = cv2.VideoWriter(
+        filename=raw_filename,
+        fourcc=cv2.VideoWriter_fourcc(*"mp4v"),
+        fps=output_video.get(cv2.CAP_PROP_FPS),
+        frameSize=(
+            int(output_video.get(cv2.CAP_PROP_FRAME_WIDTH)),
+            int(output_video.get(cv2.CAP_PROP_FRAME_HEIGHT)),
+        ),
+    )
     for results in result:
         annotated_frame = results.plot()
         annotated_video.write(annotated_frame)
@@ -115,19 +131,29 @@ async def detect_video(file: UploadFile):
     annotated_video.release()
     for frame_result in result:
         frame_boxes = frame_result.boxes
-        for i in range (len(frame_result.boxes)):
+        for i in range(len(frame_result.boxes)):
             class_name = model.names[int(frame_boxes.cls[i])]
             class_counts[class_name] = class_counts.get(class_name, 0) + 1
 
-    subprocess.run([
-        "ffmpeg", "-y", "-i", raw_filename,
-        "-vcodec", "libx264", "-pix_fmt", "yuv420p",
-        output_filename
-    ], check=True)
-    os.remove (temp_path)
-    os.remove (raw_filename)
+    subprocess.run(
+        [
+            "ffmpeg",
+            "-y",
+            "-i",
+            raw_filename,
+            "-vcodec",
+            "libx264",
+            "-pix_fmt",
+            "yuv420p",
+            output_filename,
+        ],
+        check=True,
+    )
+    os.remove(temp_path)
+    os.remove(raw_filename)
     return {"summary": class_counts, "annotated_video": output_filename}
-    
-@app.get ("/api/results/{filename}")
-def get_result (filename: str):
+
+
+@app.get("/api/results/{filename}")
+def get_result(filename: str):
     return FileResponse(filename)
